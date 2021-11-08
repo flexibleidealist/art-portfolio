2bb22803f4628cc8d881c2551c8e84d4de0846e2
Nicholas Rynearson <nrynearson@gmail.com>
2021-11-08 14:52:35 -0500

    adds fade-in effect to cover images in Carousel component; still not perfect but progress

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,35 +1,42 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import works from "../../works.json"
 import "./Carousel.css"
 
 function Carousel(){
-  // const [currentWork, setCurrentWork] = useState({})
-  // let i = 0
-  // const advanceImage = () => {
-  //   i < works.length ? i++ : i = 0
-  //   setCurrentWork(works[i])
-  // }
-  // setInterval(advanceImage, 3000)
+  const [currentCover, setCurrentCover] = useState({ "title": "Cynic",
+  "year": 2019,
+  "materials": "paper",
+  "image": "https://i.imgur.com/iwGQ8MT.jpg",
+  "gallery": "collage",
+  "id": 5,
+  "coverImage": true})
+  const [newCover, setNewCover] = useState(true)
+  let i = 0
+  const coverImages = works.filter(work => work.coverImage === true)
+  const changeCover = () => {
+    i < coverImages.length -1 ? i++ : i = 0;
+    setNewCover(true)
+    setCurrentCover(coverImages[i]);
+    setTimeout(()=> {
+      setNewCover(false)
+    }, 4000)
+  }
+  useEffect(() => {
+    setInterval(() => {
+      changeCover()
+    }, 11000)
+  }, [])
 
-//   let i = 0;
-//   const slideShow = () => {
-//   const image = document.getElementById('slide');
-//   image.src = imageArr[i];
-//   image.classList.add('fade');
-//   i++;
-//   setTimeout(() => {
-//     image.classList.remove('fade');
-//   }, 3500);
-//   if (i === imageArr.length) {
-//     i = 0;
-//   }
-// }
-//   setInterval(slideShow, 6000);
-
-  return (
+  
+  return currentCover ? 
+  (
     <div className="carousel">
-      <img src="https://i.imgur.com/iwGQ8MT.jpg" alt="collage entitled Cynic" />
-      {/* <img src={currentWork.image} alt={`${currentWork.title} from the ${currentWork.gallery} gallery`} /> */}
+      <img className={newCover ? "fade-in" : "fade-out"} src={currentCover?.image} alt={`${currentCover.gallery} entitled ${currentCover.title}`} />
+    </div>
+  )
+  : (
+    <div>
+      <h3>loading...</h3>
     </div>
   )
 }
